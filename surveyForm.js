e22f8aa5974f1fba42bf4273422f84b7107ab101
Ian <ian (at) ianrobinson.net>
2018-06-19 12:41:19 -0700

    Set email verification using HTML5 rather than JS

--- surveyForm.js
+++ surveyForm.js
@@ -3,20 +3,4 @@ window.onload = go;
 function go() {
   console.log(validate("ianrobinson@ian.net"), " <- true? Good, validate working");
   console.log(validate("ianrobinson@ian.ne!"), " <- false? Good, validate working");
-  
-  // document.addEventListener("DOMContentLoaded", function() {
-  //   var elements = document.getElementById("INPUT");
-  //   for (var i = 0; i < elements.length; i++) {
-  //     elements[i].oninvalid = function(e) {
-  //         e.target.setCustomValidity("");
-  //         if (!e.target.validity.valid) {
-  //             e.target.setCustomValidity("This field cannot be left blank");
-  //         }
-  //     };
-  //     elements[i].oninput = function(e) {
-  //         e.target.setCustomValidity("");
-  //     };
-  //   }
-  });
-
 }
