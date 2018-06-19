7e08e46fde0b87e8b0273d52bfa370f8ce23defd
Ian <ian (at) ianrobinson.net>
2018-06-19 14:55:41 -0700

    Alert is pulling data from user now, but need to make the radio buttons and check marks read user data rather than undefined

--- surveyForm.js
+++ surveyForm.js
@@ -1,4 +1,4 @@
-var lovedOneInput, emailInput, favNumInput, selectedEleInput, userMessageInput;
+var lovedOneInput, emailInput, favNumInput, selectedEleInput, focusInput, goodThingsInput, userMessageInput;
 var nameOfLovedOne, email, favNum, selectedEle, focusArea, gratitudeCheckedBoxes, userMessage;
 window.onload = go;
 
@@ -7,6 +7,9 @@ function go() {
   emailInput = document.getElementById("email");
   favNumInput = document.getElementById("number");
   selectedEleInput = document.getElementById("dropdown");
+  focusInput = document.getElementsByName("contact");
+  goodThingsInput = document.getElementsByName("good-things");
+  console.log(focusInput[1], goodThingsInput[1])
   userMessageInput = document.getElementById("self-generated-nice-thing");
   // console.log(lovedOneInput, emailInput, favNumInput, selectedEleInput, userMessageInput);
 }
@@ -15,7 +18,22 @@ function submissionClick() {
   nameOfLovedOne = lovedOneInput.value;
   email = emailInput.value;
   favNum = favNumInput.value;
-  alert(`Remember that ${nameOfLovedOne} loves you and your number ${favNum} symbolizes luck`);
+  selectedEle = selectedEleInput.value;
+  focus = focusInput.value;
+  goodThings = goodThingsInput.value;
+  console.log(focusInput[1], goodThingsInput[1])
+  userMessage = userMessageInput.value;
+  alert(`
+    Remember that ${nameOfLovedOne} loves you and 
+    your number ${favNum} symbolizes luck. The power
+    element of ${selectedEle} will support you this
+    in the short term.
+    It's good that you're focused on ${focus}.
+    Don't forget the things you have to be grateful for:
+    ${goodThings}.
+    Your words are powerful. Here they are: 
+    ${userMessage}
+    `);
 }
 
 function gratitudeMessageGenerator() {
